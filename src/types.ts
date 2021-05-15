0d179a7adcc0ad829c4ccec6e9e7d11bb3fdd88c
varunm0503 <varunm0503@gmail.com>
2021-05-15 15:32:14 +0530

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -6,6 +6,7 @@ export interface IProduct {
     description?: string
 }
 
+//review: CartItem can only have reference to product id and quantity and we can read all other prodct details by finding the product from products array. this will avoid double source of truth
 export interface ICartItem {
     id: string,
     name: string,
